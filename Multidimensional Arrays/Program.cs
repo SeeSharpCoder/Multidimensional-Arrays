b514997ceb3110549bf97aea8958b92e15793474
SeeSharpCoder <ab_5@abv.bg>
2018-02-01 20:20:54 +0200

    test

---
+++ Multidimensional Arrays/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multidimensional_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowsAndColumns = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.None).Select(int.Parse).ToArray();
+            int[,] matrix = new int[rowsAndColumns[0], rowsAndColumns[1]];
+
+            for (int rows = 0; rows < rowsAndColumns[0]; rows++)
+            {
+                int[] rowValues = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.None).Select(int.Parse).ToArray();
+
+                for (int columns = 0; columns < rowsAndColumns[1]; columns++)
+                {
+                    matrix[rows, columns] = rowValues[columns];
+                }
+            }
+
+            int sum = 0;
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int columns = 0; columns < matrix.GetLength(1); columns++)
+                {
+                    sum += matrix[rows, columns];
+                }
+            }
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sum);
+        }
+    }
+}
