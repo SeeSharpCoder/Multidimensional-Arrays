ae186c60c51c700153943a990153d756d109fc63
plamen.todorov <ab_5@abv.bg>
2018-02-22 13:37:33 +0200

    dfa

--- C# OOP Basics/20180213_Exercise_Defining_Classes/02.Creating_Constructors/Program.cs
+++ C# OOP Basics/20180213_Exercise_Defining_Classes/02.Creating_Constructors/Program.cs
@@ -6,7 +6,7 @@ namespace _02.Creating_Constructors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello faf!");
         }
     }
 }
