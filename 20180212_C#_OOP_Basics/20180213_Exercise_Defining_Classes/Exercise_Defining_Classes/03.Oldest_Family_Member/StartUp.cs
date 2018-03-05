6a04ea60cd4d58a4e38ae963ec35286e5ed70e97
ab_5 <ab_5@abv.bg>
2018-03-05 17:33:16 +0200

    Update

---
+++ 20180212_C#_OOP_Basics/20180213_Exercise_Defining_Classes/Exercise_Defining_Classes/03.Oldest_Family_Member/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.Oldest_Family_Member
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
